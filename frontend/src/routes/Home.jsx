97ffe322ff925cbd0d43617713e5670021baad80
Tindy Natasha <tindynatasha@gmail.com>
2024-03-30 18:19:03 +0300

    Django models created

---
+++ frontend/src/routes/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const Home = () => {
+  return (
+    <div className="container-fluid">
+           
+    </div>
+  )
+}
+
+export default Home
